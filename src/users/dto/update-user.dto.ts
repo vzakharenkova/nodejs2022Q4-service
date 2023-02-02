9c9e575b564b91d56a6d08b4d3a17eb6bcf5aee3
vzakharenkova <v.s.zakharenkova@gmail.com>
2023-02-02 17:58:19 +0300

    fix: fix change dto user type

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,8 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
 import { IsNotEmpty, IsString } from 'class-validator';
-import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
+export class UpdateUserDto {
   @IsNotEmpty()
   @IsString()
   readonly oldPassword: string;
