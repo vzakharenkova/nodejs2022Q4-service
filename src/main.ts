b709394f0f93c38e11e8b142998b824655c1eadb
vzakharenkova <v.s.zakharenkova@gmail.com>
2023-02-02 17:38:17 +0300

    feat: add exception filter and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { BadRequestException, ValidationError, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        const customError = errors
+          .map((error) => {
+            const keys = Object.keys(error.constraints);
+
+            return keys.map((key) => error.constraints[key]).join(', ');
+          })
+          .join('; ');
+
+        return new BadRequestException(customError);
+      },
+    }),
+  );
   await app.listen(4000);
 }
 bootstrap();
