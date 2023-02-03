1a8753f424c687f282c4de7efc0ffdc1d0a13ba5
vzakharenkova <v.s.zakharenkova@gmail.com>
2023-02-03 16:15:24 +0300

    feat: use .env file

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { BadRequestException, ValidationError, ValidationPipe } from '@nestjs/co
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -21,6 +25,6 @@ async function bootstrap() {
       },
     }),
   );
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
